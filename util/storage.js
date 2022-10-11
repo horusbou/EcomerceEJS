2e1b5250d90b7b2c957ce4354475890db325271e
horusbou <bouqhamza@gmail.com>
2022-10-11 21:47:15 +0100

    uploading folders

--- util/storage.js
+++ util/storage.js
@@ -9,11 +9,6 @@ cloudinary.config({
   api_secret: process.env.API_SECRET
 })
 
-console.log({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET
-})
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
